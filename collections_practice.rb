da214dc26cf644127e7b862f67e766e68a6b8ab3
Daniel Ran <danielr1735@gmail.com>
2018-06-26 16:12:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,7 @@
 def sort_array_asc(int_arr)
   sorted = []
   while int_arr.length > 0
-  sorted.push(int_arr.delete(int_arr.min))
+    sorted.push(int_arr.delete(int_arr.min))
+  end
   sorted
 end

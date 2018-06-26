9ba1746f27f62fc66889e98c8fec3d3973222265
Daniel Ran <danielr1735@gmail.com>
2018-06-26 16:13:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,3 +6,11 @@ def sort_array_asc(int_arr)
   end
   sorted
 end
+
+def sort_array_desc(int_arr)
+  sorted = []
+  while int_arr.length > 0
+    sorted.push(int_arr.delete(int_arr.max))
+  end
+  sorted
+end
